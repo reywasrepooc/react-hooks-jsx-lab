3a31961e99170ca8f99e93c244e79feae0d065fc
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-06-29 11:34:10 -0400

    added some style

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2> About me</h2>
+    <p>I like long walks on the beach</p>
+    <img src= {image} alt="I made this" />
+
+  </div>;
 }
 
 export default About;
